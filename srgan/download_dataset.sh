a662b01b664f653422dd0205740686f521582cf7
eriklindernoren <eriklindernoren@gmail.com>
2018-03-01 01:24:42 +0100

    SRGAN: Removed download_dataset.sh

--- srgan/download_dataset.sh
+++
@@ -1,9 +0,0 @@
-mkdir datasets
-FILE=tiny-imagenet-200
-URL=http://cs231n.stanford.edu/tiny-imagenet-200.zip
-TAR_FILE=./datasets/$FILE.tar.gz
-TARGET_DIR=./datasets/$FILE/
-wget -N $URL -O $TAR_FILE
-mkdir $TARGET_DIR
-tar -zxvf $TAR_FILE -C ./datasets/
-rm $TAR_FILE
