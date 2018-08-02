bdd3c40bb4cf8f4c28535562958a4d53ead7977a
Ero98 <oujghg@163.com>
2018-08-02 17:43:07 +0800

    Update bgan.py

--- bgan/bgan.py
+++ bgan/bgan.py
@@ -36,7 +36,7 @@ class BGAN():
         self.generator = self.build_generator()
 
         # The generator takes noise as input and generated imgs
-        z = Input(shape=(100,))
+        z = Input(shape=(self.latent_dim,))
         img = self.generator(z)
 
         # For the combined model we will only train the generator
