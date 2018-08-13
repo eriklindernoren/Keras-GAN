9fbf3ecea438b8132dfa55f70ee2c894932b3c21
ramsey.barghouti@gmail.com <ramsey@addgene.org>
2018-08-13 12:57:23 -0400

    factored gradient_penalty_loss out of class

--- keras_gan/wgan_gp.py
+++ keras_gan/wgan_gp.py
@@ -35,6 +35,24 @@ def wasserstein_loss(y_true, y_pred):
     return K.mean(y_true * y_pred)
 
 
+def gradient_penalty_loss(_, y_pred, averaged_samples):
+    """
+    Computes gradient penalty based on prediction and weighted real / fake samples
+    """
+    gradients = K.gradients(y_pred, averaged_samples)[0]
+    # compute the euclidean norm by squaring ...
+    gradients_sqr = K.square(gradients)
+    #   ... summing over the rows ...
+    gradients_sqr_sum = K.sum(gradients_sqr,
+                              axis=np.arange(1, len(gradients_sqr.shape)))
+    #   ... and sqrt
+    gradient_l2_norm = K.sqrt(gradients_sqr_sum)
+    # compute lambda * (1 - ||grad||)^2 still for each single sample
+    gradient_penalty = K.square(1 - gradient_l2_norm)
+    # return the mean as loss over all the batch samples
+    return K.mean(gradient_penalty)
+
+
 class WGANGP(GANBase):
     def __init__(
             self,
@@ -97,7 +115,7 @@ class WGANGP(GANBase):
 
         # Use Python partial to provide loss function with additional
         # 'averaged_samples' argument
-        partial_gp_loss = partial(self.gradient_penalty_loss,
+        partial_gp_loss = partial(gradient_penalty_loss,
                                   averaged_samples=interpolated_img)
         partial_gp_loss.__name__ = 'gradient_penalty'  # Keras requires function names
 
@@ -192,23 +210,6 @@ class WGANGP(GANBase):
     def save(self):
         self.save_config()
         self.save_model()
-
-    def gradient_penalty_loss(self, y_true, y_pred, averaged_samples):
-        """
-        Computes gradient penalty based on prediction and weighted real / fake samples
-        """
-        gradients = K.gradients(y_pred, averaged_samples)[0]
-        # compute the euclidean norm by squaring ...
-        gradients_sqr = K.square(gradients)
-        #   ... summing over the rows ...
-        gradients_sqr_sum = K.sum(gradients_sqr,
-                                  axis=np.arange(1, len(gradients_sqr.shape)))
-        #   ... and sqrt
-        gradient_l2_norm = K.sqrt(gradients_sqr_sum)
-        # compute lambda * (1 - ||grad||)^2 still for each single sample
-        gradient_penalty = K.square(1 - gradient_l2_norm)
-        # return the mean as loss over all the batch samples
-        return K.mean(gradient_penalty)
 
     def build_generator(self):
 
