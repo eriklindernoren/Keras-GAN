43b06f7a51afd6b6f0ce8b6c1f2969ee225044ba
ramsey.barghouti@gmail.com <ramsey@addgene.org>
2018-08-12 22:57:41 -0400

    Added very basic setup.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='keras_gan',
+      version='0.1',
+      description='Several GAN implementations',
+      author='Ramsey Barghouti',
+      license='GPL3',
+      packages=['keras_gan'],
+      zip_safe=False)
