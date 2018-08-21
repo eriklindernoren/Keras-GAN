cf14fa1650f0b3c9f53281e44215c41323d2ba02
ramsey.barghouti@gmail.com <ramsey@addgene.org>
2018-08-21 08:41:26 -0400

    updated setup with install requires.

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
 
-setup(name='keras_gan',
-      version='0.1',
-      description='Several GAN implementations',
-      author='Erik Lindernoren',
-      license='MIT',
-      packages=['keras_gan'],
-      zip_safe=False)
+setup(
+    name='keras_gan',
+    version='0.1',
+    description='Several GAN implementations',
+    author='Erik Lindernoren',
+    license='MIT',
+    packages=['keras_gan'],
+    zip_safe=False,
+    install_requires=[
+        "numpy",
+        "keras"
+    ],
+)
