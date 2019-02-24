0935156d743ad05664cd839aa5c2025ce266e61e
kurapan <matteas@ymail.com>
2019-02-24 20:25:53 +0900

    updated instance normalization import

--- discogan/discogan.py
+++ discogan/discogan.py
@@ -2,7 +2,7 @@ from __future__ import print_function, division
 import scipy
 
 from keras.datasets import mnist
-from keras_contrib.layers.normalization import InstanceNormalization
+from keras_contrib.layers.normalization.instancenormalization import InstanceNormalization
 from keras.layers import Input, Dense, Reshape, Flatten, Dropout, Concatenate
 from keras.layers import BatchNormalization, Activation, ZeroPadding2D
 from keras.layers.advanced_activations import LeakyReLU
