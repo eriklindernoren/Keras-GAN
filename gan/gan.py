c7dade982df8cb5d068a6ae1caee2a89dc8b443a
stanleycai123 <32364021+stanleycai123@users.noreply.github.com>
2022-01-23 12:09:33 -0500

    Needs to change from "keras" to "tensorflow.keras"

--- gan/gan.py
+++ gan/gan.py
@@ -6,7 +6,7 @@ from keras.layers import BatchNormalization, Activation, ZeroPadding2D
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.models import Sequential, Model
-from keras.optimizers import Adam
+from tensorflow.keras.optimizers import Adam
 
 import matplotlib.pyplot as plt
 
